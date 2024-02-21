7c348a07e91ac541c35ede3dabd8790af745f7c2
ITAssassin8 <125161672+ITAssassin8@users.noreply.github.com>
2024-02-21 21:40:40 +0330

    Update and rename P3class to P3class.c

--- P3class.c
+++ P3class.c
@@ -5,7 +5,7 @@ int main() {
     char in_str[100];
     scanf("%s", in_str);
 
-    char out_str[5][10] = {0};
+    char out_str[5][10] = {0}; // Initialize all elements to null characters
     
     int len = strlen(in_str);
     int j = 0;
@@ -15,6 +15,7 @@ int main() {
         if (in_str[i] == ',') {
             j++;
             k = 0; // Reset k for the next substring
+            in_str[i] = '@';
         } else {
             out_str[j][k] = in_str[i];
             k++;
@@ -23,7 +24,10 @@ int main() {
 
     printf("index 0: %s\n", out_str[0]);
     printf("index 1: %s\n", out_str[1]);
-    printf("index 2: %s\n", out_str[2]);
+    printf("index 2: %s\n\n", out_str[2]);
+    
+    printf("%s", in_str);
 
     return 0;
 }
+
